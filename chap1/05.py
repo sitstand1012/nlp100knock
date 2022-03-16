2cbdc3872859228c5ee4ad1f424a4584f71eadcd
sitstand1012 <saccubusonly2012@gmail.com>
2022-03-16 21:05:57 +0900

    reconsidered char-level n-gram

--- chap1/05.py
+++ chap1/05.py
@@ -7,24 +7,28 @@
 #文字ごとのbigram
 def char_bigram(s):
     l=[]
-    for i in range(len(s)-1):
-        #[BOS]+1文字目のbi-gramを作る
-        if i==0:
-            l.append("".join(["_",s[i]]))
-        #i文字目+(i+1)文字目のbi-gram
-        l.append("".join([s[i],s[i+1]]))
-    #最終文字+[EOS]のbi-gramを作る
-    l.append("".join([s[-1], "_"]))
+
+    #単語列に
+    wlist=s.split()
+
+    #単語ごとに
+    for word in wlist:
+        #[BOW]+1文字目のbi-gram
+        l.append("".join(["_", word[0]]))
+        for i in range(len(word)-1):
+            #i文字め+(i+1)文字めのbi-gram
+            l.append("".join([word[i], word[i+1]]))
+        #最後の文字+[EOW]のbi-gram
+        l.append("".join([word[-1], "_"]))
     return l
 
 #単語ごとのbigram
 def word_bigram(s):
     l=[]
     wlist=s.split()
+    #[BOS]+最初の単語のbi-gramを作る
+    l.append(["[BOS]", wlist[0]])
     for i in range(len(wlist)-1):
-        #[BOS]+1単語めのbi-gramを作る
-        if i==0:
-            l.append(["[BOS]",wlist[i]])
         #i単語目+(i+1)単語目のbi-gram
         l.append([wlist[i],wlist[i+1]])
     #最終単語+[EOS]のbi-gramを作る
